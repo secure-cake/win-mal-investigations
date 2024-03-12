24ea37ee3109488ac349764e2c3ef1401a5eb6ff
Secure Cake <49917607+secure-cake@users.noreply.github.com>
2024-03-12 11:53:03 -0400

    Create WinMal-Target-Excel.ps1

---
+++ win-mal-separate-scripts/WinMal-Target-Excel.ps1
@@ -0,0 +1,35 @@
+#change four variables below for casename, target hostname, triage dir and output dir
+$casename = 'wmi-case'
+$target_hostname = 'target'
+$triage_data_directory = "d:\cases\$casename\wmi_data"
+$output_destination_directory = "d:\cases\$casename\output"
+
+(get-childitem -Directory $triage_data_directory).name | ForEach-Object {
+#Copies triage-collection "results" to kape_output directory for review
+Copy-Item $triage_data_directory\$_\results\*Netstat.csv  $output_destination_directory\$target_hostname'-netstat.csv'
+Copy-Item $triage_data_directory\$_\results\*StartupItems.csv  $output_destination_directory\$target_hostname'-startup.csv'
+Copy-Item $triage_data_directory\$_\results\*pslist.csv  $output_destination_directory\$target_hostname'-pslist.csv'
+Copy-Item $triage_data_directory\$_\results\*Services.csv  $output_destination_directory\$target_hostname'-services.csv'
+Copy-Item $triage_data_directory\$_\results\*Scheduler*.csv  $output_destination_directory\$target_hostname'-tasks.csv'
+Copy-Item $triage_data_directory\$_\results\*DNSCache.csv  $output_destination_directory\$target_hostname'-dns.csv'
+}
+#if prompted to save in Excel, click "don't save"
+(get-childitem -Directory $triage_data_directory).name | ForEach-Object {
+    $ExcelObject=New-Object -ComObject excel.application
+    $ExcelObject.visible=$true
+    $ExcelFiles=Get-ChildItem -Path $output_destination_directory -Recurse -Include *.csv, *.xls, *.xlsx
+
+    $Workbook=$ExcelObject.Workbooks.add()
+    $Worksheet=$Workbook.Sheets.Item("Sheet1")
+
+    foreach($ExcelFile in $ExcelFiles){
+ 
+        $Everyexcel=$ExcelObject.Workbooks.Open($ExcelFile.FullName)
+        $Everysheet=$Everyexcel.sheets.item(1)
+        $Everysheet.Copy($Worksheet)
+    $Everyexcel.Close()
+ 
+    }
+$Workbook.SaveAs("$output_destination_directory\target-with-baseline-output.xlsx")
+$ExcelObject.Quit()
+}
